e6a3e8d60ef2b689de96717db6f4e0df7077bbb2
chenalian <1020484242@qq.com>
2022-02-22 14:14:45 +0800

    第四次提交

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 # Blog-SpringBoot-Vue
+```
 基于vue和springboot的博客系统
-本项目是借鉴于：https://gitee.com/zhang_hanzhe/blog
-项目的演示地址：
-项目详解地址：
-项目的部署：
-git clone 
+```
+>本项目是借鉴于：https://gitee.com/zhang_hanzhe/blog
+
+>项目的演示地址：
+
+>项目详解地址：
+#### 项目的部署：
+>1，git clone git@github.com:chenalian/Blog-SpringBoot-Vue.git
+
+>2.打开springboot-后端中的application.yml配置好数据库和redis，并将数据库文件导入
+
+>3. 打开vue-前端，用命令行打开,并输入命令
+> ```
+> npm install   //下载依赖
+> npm run dev  //启动项目
+> ```
---
+++ vue-Front/src/service/constant.js
@@ -0,0 +1,20 @@
+// AES解密参数
+export const AES_KEY = "1234567890hijklm";
+export const AES_IV = "1234567890abcdef";
+// 请求服务器地址
+export const REQUEST_BASE_URL = "http://127.0.0.1:8080/api";
+// export const REQUEST_BASE_URL = "/api";
+export const REQUEST_TIMEOUT = 8000;
+// 响应状态码
+export const RESULT_CODE = {
+    // 处理成功状态码
+    SUCCESS: 100,
+    // 找不到博客
+    BLOG_NOT_FOUND: 201,
+    // Token处理失败
+    TOKEN_ERROR: 202
+};
+// 分页消息提示
+export const NOT_PRE_PAGE_MESSAGE = "已经是第一页了";
+export const NOT_NEXT_PAGE_MESSAGE = "已经是最后一页了";
+export const PAGE_NUM_FAIL_MESSAGE = "页码不合法或不在范围内";
